be98495a7e51a4dcd4703380ef0309e0b9bf3f72
taporian <christapor.har@gmail.com>
2024-04-26 23:52:49 +0300

    chore/ updated mongo to be local

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,34 @@
-import express  from "express";
+import express from "express";
 import mongoose from "mongoose";
 
-import key from './db/config.js';
-import authRouter from './router/api/auth.js';
-import applyPassportStrategy  from "./middleware/passport.js";
-import passport from 'passport';
-import cors from 'cors';
-//Connect to MongoDB 
-mongoose.connect(key.MONGO_URI, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true, }).then(
-    () => {console.log('Connected to Movie DB') },
-    err => { console.log('Can not connect to the database'+ err)}
-);
+import key from "./db/config.js";
+import authRouter from "./router/api/auth.js";
+import applyPassportStrategy from "./middleware/passport.js";
+import passport from "passport";
+import cors from "cors";
 
 const app = express();
-app.use(
-	cors()
-);
-// Authorization 
-applyPassportStrategy(passport);
-app.use(passport.initialize());
 
-const PORT = process.env.PORT || 5000 ;
+// Connect to MongoDB
+mongoose
+  .connect(key.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to Movie DB");
 
+    // Middleware
+    app.use(cors());
     app.use(express.json());
-    app.use("/api",authRouter);
-    
+    app.use(passport.initialize());
+    applyPassportStrategy(passport);
 
-    app.listen(PORT,()=>{
-        console.log(`Server started on ${PORT}` );
+    // Routes
+    app.use("/api", authRouter);
+
+    const PORT = process.env.PORT || 3002;
+    app.listen(PORT, () => {
+      console.log(`Server started on ${PORT}`);
     });
-
-   
+  })
+  .catch((err) => {
+    console.log("Can not connect to the database", err);
+  });
